8432de6edd98b034bf8f87202e9e0d318ada9564
henriquegibi <henriquedepadua@yahoo.com.br>
2020-09-21 21:36:35 -0300

    Criando botao MapGenerator para facilitar a producao

---
+++ Assets/Scripts/Editor/EditorGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Generator))]
+public class EditorGenerator : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Generator generator = (Generator)target;
+
+        if (GUILayout.Button("Generate Map"))
+        {
+            generator.GenerateMap();
+        }
+
+        if (GUILayout.Button("Clean Map"))
+        {
+            generator.CleanMap();
+        }
+    }
+}
---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{    
+    public void GenerateMap()
+    {
+        Debug.Log("Generate map");
+    }
+
+    public void CleanMap()
+    {
+        Debug.Log("Clean Map");
+    }
+}
