31531645e106c7dd2ee7e3c5d3618d221d2d7370
Henrique Gibi <henriquedepadua@yahoo.com.br>
2020-11-01 12:58:14 -0300

    Retomando o curso

---
+++ Assets/Scripts/Editor/GeneradorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Runtime;
+
+[CustomEditor(typeof(Generador))]
+public class GeneradorEditor : Editor
+{
+   public override void OnInspectorGUI()
+    {
+        Generador generador = (Generador)target;
+
+        if(GUILayout.Button("Generar Mapa"))
+        {
+            generador.GenerarMapa();
+        }
+
+        if(GUILayout.Button("Limpiar Mapa"))
+        {
+            generador.LimpiarMapa();
+        }
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/Generador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using UnityEngine;
+
+public class Generador : MonoBehaviour
+{
+    public void GenerarMapa()
+    {
+        print("Generar mapa");
+    }
+
+    public void LimpiarMapa()
+    {
+        print("Limpiar mapa");
+    }
+}
